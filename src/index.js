40e2d7bded4c7c48e3eca48574cb479681f24e39
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-09 22:51:46 +0900

    add react intersection observer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import './index.css';
 
 const Box = () => {
-	return <div className="Box"></div>;
+	const controls = useAnimation();
+	const { ref, inView } = useInView();
+
+	useEffect(() => {
+		if (inView) {
+			controls.start('visible');
+		}
+		if (!inView) {
+			controls.start('hidden');
+		}
+	}, [controls, inView]);
+
+	const boxVariants = {
+		hidden: { scale: 0 },
+		visible: {
+			scale: 1,
+			transition: {
+				duration: 0.5,
+			},
+		},
+	};
+
+	return (
+		<motion.div
+			ref={ref}
+			className="Box"
+			initial="hidden"
+			animate={controls}
+			variants={boxVariants}
+		></motion.div>
+	);
 };
 
 const App = () => {
